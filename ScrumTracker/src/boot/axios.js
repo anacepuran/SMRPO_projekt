90206b841566f91bc073264f91d7b0eda7c2e02f
Ana Cepuran <anacepuran@Anas-MacBook-Air.local>
2021-03-17 15:54:41 +0100

    Ready to deploy!

--- ScrumTracker/src/boot/axios.js
+++ ScrumTracker/src/boot/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export default ({ app, store }) => {
   const instance = axios.create({
-    baseURL: 'http://127.0.0.1:5000/',
+    baseURL: 'http://127.0.0.1:8000/',
     withCredentials: false
   })
 
--- backend/app.py
+++ backend/app.py
@@ -3,14 +3,17 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
-from routes.project import projects_route
-from routes.cards import cards_route
-from routes.scrums import scrums_route
-from routes.sprints import sprints_route
-from routes.users import users_route
-from db import mongo
+from backend.db import mongo
+import os
+from dotenv import load_dotenv
+from backend.routes.project import projects_route
+from backend.routes.cards import cards_route
+from backend.routes.scrums import scrums_route
+from backend.routes.sprints import sprints_route
+from backend.routes.users import users_route
+load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../ScrumTracker/dist/',    static_url_path='/')
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['MONGO_URI'] = 'mongodb+srv://admin:smrpoadmin@clustertest.ef9bm.mongodb.net/testdb?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE'
 mongo.init_app(app)
@@ -23,10 +26,12 @@ app.register_blueprint(sprints_route)
 app.register_blueprint(cards_route)
 
 
+# Set up the index route
 @app.route('/')
-def test_api():
-    return "Api works!"
+def index():
+    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
